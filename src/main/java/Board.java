acc9597b5e6b534c159a904c6d15b2e27dbd12d3
Lavreen <lszymo@gmail.com>
2020-12-15 23:23:52 +0100

    Version 0

---
+++ src/main/java/Board.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Board {
+
+    private Field[][] fields;
+
+    private int size;
+
+    public Board(int size){}
+
+    private void createFields(){}
+
+    private void setFieldsNeighbours() {}
+
+    public ArrayList<Field> getFields(){return null;}
+
+    public Boolean isWinner(Player player){return false;}
+}
