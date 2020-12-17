6d8886363e19e5e3fcaacff39107e64dd65d7029
Lavreen <lszymo@gmail.com>
2020-12-17 19:23:35 +0100

    Scaffolding for Field class and board generation; first part of the board able to be generated. createFields method's name changed to generateBoard.

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -2,13 +2,59 @@ import java.util.ArrayList;
 
 public class Board {
 
-    private Field[][] fields;
+    private boolean[][] grid; // "true" - field exists
+    public Field[][] fields;
+    final private int SIZE;
+    public Board(int size) {
+        SIZE = size;
+    }
 
-    private int size;
+    /**
+     *  yellow      red       black
+     *
+     *              HEX
+     *
+     *  white       green       blue
+     */
+    private void generateBoard() {
+        final int X = 7 + 6 * (SIZE - 1);
+        final int Y = 5 + 4 * (SIZE - 1);
 
-    public Board(int size){}
+        // red triangle generation
+        final int redOriginX = X / 2 + 1; // coordinates of the vertex from which the triangle generation starts
+        final int redOriginY = 0;         //
+        int x = redOriginX;
+        int y = redOriginY;
+        int rowBeginningX, rowBeginningY;
+        int count = 1;
+        for (int i = 0; i < SIZE; i++) {
+            rowBeginningX = x;
+            rowBeginningY = y;
+            for (int j = 0; j < i+1; j++) {
+                grid[x][y] = true;
+                fields[x][y] = new Field('R', count, x, y);
+                count++;
+                if(j < i) {
+                    x += 2;
+                }
+                else {
+                    x = rowBeginningX - 1;
+                    y = rowBeginningY + 1;
+                }
+            }
 
-    private void createFields(){}
+        }
+    }
+
+
+
+
+
+
+
+
+
+//    private void createFields(){} ---> generateBoard
 
     private void setFieldsNeighbours() {}
 
--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -1,32 +1,42 @@
 import java.util.ArrayList;
 
 public class Field {
-    private ArrayList<Field> neighbours;
-    private Player owner;
-    private Player target;
-    /*
-    Wpisałem narazie x,y. Jak będziesz to robił to zamień na twój pomysł na numeracje.
-     */
-    private int x;
-    private int y;
+
+    public Field (char color, int number, int gridCoordinateX, int gridCoordinateY)
+    {
+
+    }
 
 
-    public Field(){}
-
-    public void addNeighbour(Field field ){}
-
-    public ArrayList<Field> getNeighbours(){return null;}
-
-    public void setOwner(Player player){}
-
-    public Player getOwner(){return null;}
 
 
-    public int getX(){return 0;}
 
-    public void setX(int x){}
-
-    public int getY(){return 0;}
-
-    public void setY(int y){}
+//    private ArrayList<Field> neighbours;
+//    private Player owner;
+//    private Player target;
+//    /*
+//    Wpisałem narazie x,y. Jak będziesz to robił to zamień na twój pomysł na numeracje.
+//     */
+//    private int x;
+//    private int y;
+//
+//
+//    public Field(){}
+//
+//    public void addNeighbour(Field field ){}
+//
+//    public ArrayList<Field> getNeighbours(){return null;}
+//
+//    public void setOwner(Player player){}
+//
+//    public Player getOwner(){return null;}
+//
+//
+//    public int getX(){return 0;}
+//
+//    public void setX(int x){}
+//
+//    public int getY(){return 0;}
+//
+//    public void setY(int y){}
 }
