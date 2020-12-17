6adef39cc2ee00da2f0cdf6f6d6267597cec4c23
Aprii00 <matz027720@gmail.com>
2020-12-17 21:00:52 +0100

    Version 0.2 Class not ready. Player can move to any empty field.

--- src/main/java/Prophet.java
+++ src/main/java/Prophet.java
@@ -3,7 +3,16 @@ public class Prophet {
 
     }
 
-    public boolean move(Field fieldFrom, Field fieldTo, Player player){
-        return true;        //for now
+    public boolean move(FieldCode fieldFrom, FieldCode fieldTo, Player player, Board board){        //for now player can fly to any free field
+
+        if(board.getFieldOwner(fieldFrom.getKey(), fieldFrom.getValue()) == player
+                && board.getFieldOwner(fieldTo.getKey(), fieldTo.getValue()) == null
+        ){
+            return true;
+        }
+
+        return false;
     }
+
+    public boolean isWinner(Player player, Board board){return board.isWinner(player);}
 }
