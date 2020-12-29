ec3b6b25bc397d177696848a1d83f4057585007f
Lavreen <lszymo@gmail.com>
2020-12-29 20:59:18 +0100

    jfx fix

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,5 +1,15 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class GUI extends Application {
 
@@ -8,7 +18,18 @@ public class GUI extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+//        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        primaryStage.setTitle("Chinese Chess");
 
+        Button button = new Button();
+        button.setText("Button");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+//        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(layout, 300, 275));
+        primaryStage.show();
     }
 }
