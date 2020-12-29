c8c88174eed916291c552ef8aa5a1d340e22005e
Aprii00 <matz027720@gmail.com>
2020-12-30 00:46:10 +0100

    GUI begin

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -6,30 +6,65 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.sql.BatchUpdateException;
+import java.util.Scanner;
 
 public class GUI extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    private Socket socket;
+    private Scanner in;
+    private PrintWriter out;
+
+    private void socketSetup(){
+        try {
+            socket = new Socket("127.0.0.1", 6666);
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
+
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+
+        socketSetup();
 //        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Chinese Chess");
 
         Button button = new Button();
-        button.setText("Button");
+        //button.setText("Button");
+        button.setShape(new Circle(100));
+        button.setMinSize(100,100);
+        button.setMaxSize(100,100);
+        button.setStyle("-fx-background-color: green;");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Hej moje imie to ");
+            }
+        });
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
 
 //        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.setScene(new Scene(layout, 300, 275));
+        primaryStage.setScene(new Scene(layout, 1000, 1000));
         primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
