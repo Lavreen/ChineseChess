09dfe62b77da9de66b415f2c4998b85e5b4de496
Aprii00 <matz027720@gmail.com>
2021-01-10 19:29:05 +0100

    Prophet Factory Test

---
+++ src/test/java/ProphetFactoryTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProphetFactoryTest {
+    @Test (expected = Exception.class)
+    public void test1() throws Exception {
+        ProphetFactory prophetFactory = new ConcreteProphetFactory();
+        Prophet prophet = prophetFactory.getProphet(3);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        ProphetFactory prophetFactory = new ConcreteProphetFactory();
+        Prophet prophet = prophetFactory.getProphet(1);
+        assertTrue(prophet instanceof Prophet_1);
+        assertFalse(prophet instanceof Prophet_2);
+    }
+
+    @Test
+    public void test3() throws Exception {
+        ProphetFactory prophetFactory = new ConcreteProphetFactory();
+        Prophet prophet = prophetFactory.getProphet(2);
+        assertFalse(prophet instanceof Prophet_1);
+        assertTrue(prophet instanceof Prophet_2);
+    }
+}
